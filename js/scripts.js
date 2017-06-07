f529a0756d95f9eb7c6134e265668dada0e27fc3
Alena Golovina and Esvethlynna Pangelinan <alena@>
2017-06-07 08:57:30 -0700

    add html, css and js to the progect

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
+// business logic
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// user interface logic
 $(document).ready(function() {
-  $('#submit_button').click(function(event){
-  event.preventDefault();
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
 
-  var wordInput = $("input#userInput").val().toLowerCase();
+    $(".year").text(year);
 
-  var wordInputArray = wordInput.split('');
-  var reverseInputArray = wordInputArray.reverse();
-  var reverseUserInput = reverseInputArray.join("");
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
 
-alert (reverseUserInput);
-
-if (wordInput === reverseUserInput) {
-  alert ("It's a palindrome!");
-} else {
-  alert ("It's not a palindrome!")
-}
-  //reverseString(wordInput);
-
-    //var vowels = ['a', 'e', 'i', 'o', 'u'];
-    //for (var i = 0; i < vowels.length; i++) {   // loop through vowels
-    //  for (var j = 0; j < letterArray.length; j++) {
-      //  if (vowels[i] === letterArray[j]) {
-        //  console.log(letterArray[j]);
-        //  wordInput = wordInput.replace(letterArray[j], '-');
+    $("#result").show();
   });
-
 });
